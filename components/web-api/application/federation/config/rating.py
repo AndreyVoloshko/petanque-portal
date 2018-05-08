5e2b354c223ebb6db757af92ff62fb74231f92ad
Andrey Voloshko <andrey.v@pixellu.com>
2018-05-08 20:38:51 +0300

    past moth for rating - test revert

--- components/web-api/application/federation/config/rating.py
+++ components/web-api/application/federation/config/rating.py
@@ -6,4 +6,4 @@ RATING_TOURNAMENT_POWER_TEAMS_COUNT = 16
 # Player power = sum(top N tournament points during last M month)
 # Team power = sum(player powers) / players count
 RATING_PLAYER_POWER_TOURNAMENTS_COUNT = 10
-RATING_PLAYER_POWER_PAST_MONTHES = 20
+RATING_PLAYER_POWER_PAST_MONTHES = 12
