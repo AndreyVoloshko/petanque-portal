1020c6f4ea385e66a0d238373c6fc69813f8f6ee
Andrey Voloshko <andrey.v@pixellu.com>
2024-10-03 14:04:35 +0200

    Team details to export json

--- components/web-api/application/federation/views/tournaments.py
+++ components/web-api/application/federation/views/tournaments.py
@@ -189,14 +189,17 @@ def tournament_teams_export(request, id):
 
             for player in team.team.players.all():
                 club_name = ""
+                clud_id = None
                 if player.current_club is not None:
                     club_name = player.current_club.name
+                    club_id = player.current_club.pk
 
                 current_team['players'].append({
                     'id': player.pk,
                     'name': player.name,
                     'surname': player.surname,
-                    'club': club_name
+                    'club': club_name,
+                    'club_id': club_id,
                 })
 
 
