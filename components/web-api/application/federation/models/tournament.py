e53a163a631c80bda2f43afc4c113e36c8c37d58
Andrey Voloshko <andrey.v@pixellu.com>
2025-03-21 19:35:03 +0100

    Team admin permissions

--- components/web-api/application/federation/models/tournament.py
+++ components/web-api/application/federation/models/tournament.py
@@ -503,19 +503,19 @@ class TeamTournamentMembership(models.Model):
 
     def is_user_has_admin_access_to_team(self, user):
         result = False
-
-        if self.tournament.main_organizer and user.pk == self.tournament.main_organizer.user.pk:
-            result = True
-
-        if user.is_superuser:
-            result = True
-
+        
         for player in self.team.players.all():
             if player.user.pk == user.pk:
                 result = True
 
         if self.tournament.is_finished():
             result = False
+
+        if self.tournament.main_organizer and user.pk == self.tournament.main_organizer.user.pk:
+            result = True
+
+        if user.is_superuser:
+            result = True
 
         return result
 
