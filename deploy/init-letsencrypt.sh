ec24aaf6afbe497ecb1fda4f78bf606266e97e44
Andrey Voloshko <andrey.v@pixellu.com>
2025-05-25 22:46:29 +0200

    init certbot

--- deploy/init-letsencrypt.sh
+++ deploy/init-letsencrypt.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
+
 domains=(portal.petanque.org.ua)
 email="andreyvoloshko@gmail.com"
 webroot="/var/www/certbot"
 
 docker compose -p "petanque-portal" \
-    run --rm certbot certonly --webroot \
-    --webroot-path=$webroot \
-    --email $email \
-    --agree-tos \
-    --no-eff-email \
-    $(for d in "${domains[@]}"; do echo -n "-d $d "; done)
-
+    run --rm certbot sh -c "
+        certbot certonly --webroot \
+            --webroot-path=$webroot \
+            --email $email \
+            --agree-tos \
+            --no-eff-email \
+            $(for d in "${domains[@]}"; do echo -n "-d $d "; done);
+        echo 'Waiting for certificate to be generated...';
+        sleep 5
+    "
