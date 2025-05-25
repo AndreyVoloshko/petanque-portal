49b816dc595175d88ba307b8d01370ab1812e400
Andrey Voloshko <andrey.v@pixellu.com>
2025-05-25 21:05:47 +0200

    init certbot

---
+++ deploy/init-letsencrypt.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+domains=(portal.petanque.org.ua)
+email="andreyvoloshko@gmail.com"
+webroot="/var/www/certbot"
+
+docker compose \
+    run --rm certbot certonly --webroot \
+    --webroot-path=$webroot \
+    --email $email \
+    --agree-tos \
+    --no-eff-email \
+    $(for d in "${domains[@]}"; do echo -n "-d $d "; done)
+
